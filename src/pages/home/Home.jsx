f2f4f639fc541bb8792881a428307602af633d73
basiacarvalho <basiacarvalho@gmail.com>
2023-08-13 11:25:59 +0200

    styling of the home component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,10 +3,18 @@ import { Phone } from 'grommet-icons';
 
 const Home = () => {
   return (
-    <main className={css.container}>
-      <h1 className={css.title}>Phone book manager</h1>
-      <Phone size="xlarge" />
-      <h2 className={css.subtitle}>Welcome</h2>
+    <main className={css.section}>
+      <div className={css.container}>
+        <h1 className={css.title}>Welcome to Phone Book Manager</h1>
+        <div className={css.icon_container}>
+          <Phone
+            className={css.icon}
+            size="xlarge"
+            color="#757575"
+            shadow="-2px -1px 12px rgba(61, 59, 59, 0.59)"
+          />
+        </div>
+      </div>
     </main>
   );
 };
