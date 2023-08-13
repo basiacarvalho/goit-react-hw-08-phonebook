305a80c76e20aabeb7057c8a76747f65e0371776
basiacarvalho <basiacarvalho@gmail.com>
2023-08-13 13:18:58 +0200

    styling of the header

--- src/components/authnav/AuthNav.jsx
+++ src/components/authnav/AuthNav.jsx
@@ -1,15 +1,31 @@
 import { NavLink } from 'react-router-dom';
 import css from './AuthNav.module.css';
+import { useLocation } from 'react-router-dom';
 
 export const AuthNav = () => {
+  const location = useLocation();
+  const isInLogin = location.pathname.includes('login');
   return (
-    <div>
-      <NavLink className={css.link} to="/register">
-        Register
-      </NavLink>
-      <NavLink className={css.link} to="/login">
-        Log In
-      </NavLink>
+    <div className={css.links}>
+      {isInLogin ? (
+        <>
+          <NavLink className={css.link} to="/register">
+            Register
+          </NavLink>
+          <NavLink className={css.linkActive} to="/login">
+            Log In
+          </NavLink>
+        </>
+      ) : (
+        <>
+          <NavLink className={css.linkActive} to="/register">
+            Register
+          </NavLink>
+          <NavLink className={css.link} to="/login">
+            Log In
+          </NavLink>
+        </>
+      )}
     </div>
   );
 };
